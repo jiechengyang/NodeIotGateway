660dc0373b535427ceab0e3467de0ac62ccc836b
jiechengyang <yangjiecheng1995@163.com>
2023-12-18 15:08:21 +0800

    init project

--- src/modbus_server_2021.js
+++
@@ -1,7 +0,0 @@
-const UtilsHelper = require('./helpers/UtilsHelper'),
-    systemConfig = UtilsHelper.loadConfig(),
-    CollectionServer = require('./libs/collectionServer')
-
-let collectionSerer = new CollectionServer(systemConfig)
-
-collectionSerer.run('modbus')
